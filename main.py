c11adbc788dccdde90e57cf5da9e18d7f643e474
DevDonut <41371020+DevDonut@users.noreply.github.com>
2023-05-24 13:34:37 +0200

    Add multiprocessed implementation of DQN

--- main.py
+++ main.py
@@ -1,20 +1,54 @@
-import gymnasium as gym
+import gym
 
+from typing import Callable
+from stable_baselines3.common.monitor import Monitor
 from stable_baselines3 import DQN
+from stable_baselines3.common.utils import set_random_seed
+from stable_baselines3.common.vec_env import SubprocVecEnv
 
-env = gym.make("CartPole-v1", render_mode="human")
 
-model = DQN("MlpPolicy", env, verbose=1)
-model.learn(total_timesteps=10000, log_interval=4)
-model.save("dqn_cartpole")
+hyperparams = {
+    "learning_rate": 6.3e-4,
+    "batch_size": 128,
+    "buffer_size": 50000,
+    "learning_starts": 0,
+    "gamma": 0.99,
+    "target_update_interval": 250,
+    "train_freq": 4,
+    "gradient_steps": -1,
+    "exploration_fraction": 0.12,
+    "exploration_final_eps": 0.1,
+    "policy_kwargs": dict(net_arch=[256, 256])
+}
 
-del model # remove to demonstrate saving and loading
 
-model = DQN.load("dqn_cartpole")
+def make_env(env_id: str, rank: int, seed: int=0) -> Callable:
+    def _init() -> gym.Env:
+        environ = gym.make(env_id)
+        environ.reset()
+        return environ
 
-obs, info = env.reset()
-while True:
-    action, _states = model.predict(obs, deterministic=True)
-    obs, reward, terminated, truncated, info = env.step(action)
-    if terminated or truncated:
-        obs, info = env.reset()
+    set_random_seed(seed)
+    return _init
+
+
+def run_experiment():
+    env_id = "LunarLander-v2"
+    num_cpu = 4
+
+    # Wrap the environment with a Monitor to log the results
+    log_dir = "/scratch/cjwever/AIDM_Convergence_1/logs/"
+    env = SubprocVecEnv([lambda: Monitor(gym.make(env_id), log_dir) for _ in range(num_cpu)])
+
+    model = DQN("MlpPolicy", env, **hyperparams, verbose=1)
+
+    # Train the agent
+    model.learn(total_timesteps=int(5e6),  progress_bar=True)
+
+    # Save the agent
+    model.save("dqn_lunar")
+    del model  # delete trained model to demonstrate loading
+
+
+if __name__ == '__main__':
+    run_experiment()
